fc75ba0c348a7f4aca3e11804de3d36528b5af4f
Takayuki Sakai <kaky0922@gmail.com>
2015-02-27 16:09:26 +0900

    Initial commit

---
+++ scripts/install_ruby.sh
@@ -0,0 +1,5 @@
+# install_ruby.sh
+
+source ~/.rvm/scripts/rvm
+rvm install 2.1.5
+rvm use 2.1.5 --default
---
+++ scripts/install_rvm.sh
@@ -0,0 +1,4 @@
+# install_rvm.sh
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+curl -L https://get.rvm.io | bash -s stable --autolibs=0
